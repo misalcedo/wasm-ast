67b613ce76745b07ca43452d3d9ca6ca5cd2da69
Miguel D. Salcedo <miguel@salcedo.cc>
2021-09-05 14:00:19 -0400

    Implement custom section as a parser combinator.

--- src/parser/sections.rs
+++ src/parser/sections.rs
@@ -2,37 +2,53 @@ use crate::parser::types::parse_function_type;
 use crate::parser::values::{parse_name, parse_u32, parse_vector};
 use crate::{Custom, FunctionType, ModuleSection};
 use nom::bytes::complete::{tag, take};
-use nom::combinator::{all_consuming, map_parser};
-use nom::IResult;
-
-/// Parses a section with the given identifier. Validates the section identified and length.
-///
-/// See <https://webassembly.github.io/spec/core/binary/modules.html#sections>
-fn parse_section(section: ModuleSection) -> impl Fn(&[u8]) -> IResult<&[u8], &[u8]> {
-    move |input| {
-        let (input, _) = tag(&[section as u8])(input)?;
-        let (input, length) = parse_u32(input)?;
-
-        take(length)(input)
-    }
-}
+use nom::combinator::{all_consuming, map, map_parser, rest};
+use nom::sequence::tuple;
+use nom::{IResult, Parser};
 
 /// Parses a WebAssembly custom section.
 ///
 /// See <https://webassembly.github.io/spec/core/binary/modules.html#binary-customsec>
 pub fn parse_custom_section(input: &[u8]) -> IResult<&[u8], Custom> {
-    let (remaining, section) = parse_section(ModuleSection::Custom)(input)?;
-    let (contents, name) = parse_name(section)?;
+    parse_section(ModuleSection::Custom, parse_custom_content)(input)
+}
 
-    Ok((remaining, Custom::new(name, Vec::from(contents))))
+/// Parses the custom content (name and bytes) of a custom section.
+fn parse_custom_content(input: &[u8]) -> IResult<&[u8], Custom> {
+    map(tuple((parse_name, rest)), |(name, contents)| {
+        Custom::new(name, Vec::from(contents))
+    })(input)
 }
 
 /// Parses a WebAssembly type section.
 ///
 /// See <https://webassembly.github.io/spec/core/binary/modules.html#binary-typesec>
 pub fn parse_type_section(input: &[u8]) -> IResult<&[u8], Vec<FunctionType>> {
-    map_parser(
-        parse_section(ModuleSection::Type),
-        all_consuming(parse_vector(parse_function_type)),
-    )(input)
+    parse_section(ModuleSection::Type, parse_vector(parse_function_type))(input)
+}
+
+/// Parses a section with the given identifier.
+///
+/// See <https://webassembly.github.io/spec/core/binary/modules.html#sections>
+fn parse_section<'input, O, P>(
+    section: ModuleSection,
+    parser: P,
+) -> impl FnMut(&'input [u8]) -> IResult<&'input [u8], O>
+where
+    P: Parser<&'input [u8], O, nom::error::Error<&'input [u8]>>,
+{
+    map_parser(parse_section_raw(section), all_consuming(parser))
+}
+
+/// Parses the raw bytes of a section with the given identifier.
+/// Validates the section identified and length.
+///
+/// See <https://webassembly.github.io/spec/core/binary/modules.html#sections>
+fn parse_section_raw(section: ModuleSection) -> impl Fn(&[u8]) -> IResult<&[u8], &[u8]> {
+    move |input| {
+        let (input, _) = tag(&[section as u8])(input)?;
+        let (input, length) = parse_u32(input)?;
+
+        take(length)(input)
+    }
 }
